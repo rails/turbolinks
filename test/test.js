6bf485203e89f9b02d39e20176f8a50be911a341
Vickash Mahabir <vickashmahabir@gmail.com>
2012-10-03 22:35:34 -0400

    Added test for async script loading

--- test/test.js
+++ test/test.js
@@ -1,3 +1,18 @@
 document.addEventListener("page:change", function() {
   console.log("page changed");
 });
+
+// Async Test
+function loadEmptyJS() {
+  if (document.location.href.match(/async.html/)) {
+    var script = document.createElement('script');
+    script.type = "text/javascript";
+    script.src = 'empty.js';
+    document.head.appendChild(script);
+    document.removeEventListener("page:change", loadEmptyJS);
+  } else {
+    document.addEventListener("page:change", loadEmptyJS);
+  }
+}
+Turbolinks.rememberInitialPage();
+loadEmptyJS();
