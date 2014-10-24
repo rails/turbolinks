3c936cb74e87d3b666c9d60fa862f756b7783aed
Sebastian Schleicher <sebastian.julius@gmail.com>
2014-10-24 11:25:44 +0200

    Added check for XMLHttpRequest to ensure redirection

--- lib/turbolinks/redirection.rb
+++ lib/turbolinks/redirection.rb
@@ -3,13 +3,16 @@ module Turbolinks
   # will respond with a JavaScript call to Turbolinks.visit(url).
   module Redirection
     extend ActiveSupport::Concern
-    
+
     def redirect_via_turbolinks_to(url = {}, response_status = {})
       redirect_to(url, response_status)
 
-      self.status           = 200
-      self.response_body    = "Turbolinks.visit('#{location}');"
-      response.content_type = Mime::JS
+      if request.headers['X-Requested-With'].eql? 'XMLHttpRequest'
+        self.status = 200
+        self.response_body = "Turbolinks.visit('#{location}');"
+        response.content_type = Mime::JS
+      end
+
     end
   end
 end
